ba028cc042d70c582ffd7603c5a18fdce5703595
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-01-16 21:00:18 +0530

    resolved warnings for netlify build

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,10 @@
-import Styles from "./Main.module.css";
 import Background1 from "../../assets/images/xalone background 2.png";
 
 const Main = () => {
   return (
     <div className="text-white bg-[#222222] w-full h-screen">
       <div>
-        <img src={Background1} className="w-screen h-screen" />
+        <img src={Background1} className="w-screen h-screen" alt="background" />
       </div>
       <div>hello</div>
     </div>
