51cee92832566af422473e911bd59817affb3320
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-01-21 01:16:23 +0530

    lottiefiles bug fixes

--- src/components/Music/ProjectComingSoon.js
+++ src/components/Music/ProjectComingSoon.js
@@ -1,6 +1,6 @@
 const ProjectCommingSoon = () => {
   return (
-    <div className="h-[250px] w-full shadow-[1px_2px_3px_4px_rgba(12,12,12,0.2)]">
+    <div className="h-[250px] w-full shadow-[0_0_0_0_rgba(0,0,0,0)] md:shadow-[1px_2px_3px_4px_rgba(12,12,12,0.2)] ">
       <iframe
         src="https://embed.lottiefiles.com/animation/16957"
         title="coming soon"
